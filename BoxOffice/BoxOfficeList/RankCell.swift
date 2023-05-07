7cc0864661b9514c44aa05d0ab67cbfab2d86850
SunnnySong <tjsghktjswls@naver.com>
2023-05-08 01:31:46 +0900

    feat: custom cell 만들기 (subtitle cell 사용 x)

--- BoxOffice/BoxOfficeList/RankCell.swift
+++ BoxOffice/BoxOfficeList/RankCell.swift
@@ -7,13 +7,82 @@
 
 import UIKit
 
-class RankCell: UICollectionViewListCell {
+final class RankCell: UICollectionViewListCell {
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    private let rankNumberTextView: UILabel = {
+        let textView = UILabel(frame: .zero)
+        textView.text = "1"
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.backgroundColor = .red
+        return textView
+    }()
+    
+    private let rankDetailTextView: UILabel = {
+        let textView = UILabel(frame: .zero)
+        textView.text = "신작"
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.backgroundColor = .blue
+        return textView
+    }()
+    
+    private let movieNameTextView: UILabel = {
+        let textView = UILabel(frame: .zero)
+        textView.text = "경관의 피"
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.backgroundColor = .yellow
+        return textView
+    }()
+    
+    private let movieDetailTextView: UILabel = {
+        let textView = UILabel(frame: .zero)
+        textView.text = "오늘 64,050 / 총 69,228"
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.backgroundColor = .systemPink
+        return textView
+    }()
+    
+    override func updateConfiguration(using state: UICellConfigurationState) {
+        super.updateConfiguration(using: state)
+
+        configureHierarchy()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    private func configureHierarchy() {
+        
+        let rankStackView: UIStackView = {
+            let stackView = UIStackView(arrangedSubviews: [rankNumberTextView, rankDetailTextView])
+            stackView.axis = .vertical
+            stackView.alignment = .center
+            stackView.distribution = .fillEqually
+            stackView.spacing = 2
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            
+            return stackView
+        }()
+        
+        let movieStackView: UIStackView = {
+            let stackView = UIStackView(arrangedSubviews: [movieNameTextView, movieDetailTextView])
+            stackView.axis = .vertical
+            stackView.alignment = .leading
+            stackView.distribution = .fillEqually
+            stackView.spacing = 2
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            
+            return stackView
+        }()
+        
+        contentView.addSubview(rankStackView)
+        contentView.addSubview(movieStackView)
+        
+        NSLayoutConstraint.activate([
+            rankStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            rankStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            rankStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 30),
+            rankStackView.widthAnchor.constraint(equalToConstant: 30),
+            
+            movieStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
+            movieStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15),
+            movieStackView.leadingAnchor.constraint(equalTo: rankStackView.trailingAnchor, constant: 20)
+        ])
     }
 }
